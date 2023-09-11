a034cdc70da145f29ce788d9e6acd7c4bab5841e
JLeiite <juliana.leiite@icloud.com>
2023-09-11 00:18:43 -0300

    Atualização exercicios AGO-21

--- EXERCICIOS/AGO-21/01/index.js
+++ EXERCICIOS/AGO-21/01/index.js
@@ -1,1 +1,10 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+const achouCovid = comentario.toLowerCase().includes('covid');
+const achouPandemia = comentario.toLowerCase().includes('pandemia');
+
+if(achouCovid || achouPandemia) {
+    console.log('Comentario bloqueado por conter palavras proibidas')
+} else {
+    console.log('Comentario autorizado.')
+}
